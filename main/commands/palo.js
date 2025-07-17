01936fec04ed3b7eb6cd495b2fe9ffa132024a9f
Jem <jeremyxarrabe@gmail.com>
2025-07-17 11:26:57 +0800

    up

--- main/commands/palo.js
+++ main/commands/palo.js
@@ -1,3 +1,5 @@
+import { EmbedBuilder } from 'discord.js';
+
 export default async function handlePalo(interaction, { paloLB }) {
   const target = interaction.options.getUser('target');
 
@@ -5,10 +7,9 @@ export default async function handlePalo(interaction, { paloLB }) {
   leaderboard[target.id] = (leaderboard[target.id] || 0) + 1;
   paloLB.save(leaderboard);
 
-  await interaction.reply({
-    content: `paluin si **${target.username}**! 👋`,
-    files: [
-      'https://cdn.discordapp.com/attachments/724279642806157439/1016906509382320168/XPBNYf6.gif?ex=687867fd&is=6877167d&hm=138e178ccbc721bf054d0990cd381b5548e4acedf25552e970f0720903466687&',
-    ],
-  });
+  const embed = new EmbedBuilder()
+    .setDescription(`paluin si **${target.username}**! 👋`)
+    .setImage('https://s14.gifyu.com/images/bKcLL.gif');
+
+  await interaction.reply({ embeds: [embed] });
 }
